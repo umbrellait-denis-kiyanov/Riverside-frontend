cd8845bd866e305f861e24d107485e17fc37bf1a
Denny Serejo <denny_serejo@hotmail.com>
2019-04-29 13:59:11 -0300

    posts endpoint

--- src/app/video_recorder/timed-review/timed-review.constants.ts
+++ src/app/video_recorder/timed-review/timed-review.constants.ts
@@ -1,4 +1,4 @@
 import { environment } from 'src/environments/environment';
 
 export const renderUrl = environment.serverUrl;
-export const postEndpoint = '/api/post/timed-review';
+export const postEndpoint = '/api/posts/timed-review';
