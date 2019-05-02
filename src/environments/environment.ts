bb321120622be3b3d16c99954f64519f8ab27aaa
Denny Serejo <denny_serejo@hotmail.com>
2019-05-02 10:18:30 -0300

    fixing url typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     serverUrl: 'https://recorder-us1.english3.com:9002',
-    s3Url: 'ttps://s3.us-west-2.amazonaws.com/english3'
+    s3Url: 'https://s3.us-west-2.amazonaws.com/english3'
   };
 declare global {
   interface Window { CONSTANTS: any; }
