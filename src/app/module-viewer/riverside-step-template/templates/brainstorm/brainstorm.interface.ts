5da0be1b1a07de5453f6078e8c543c1dded4dbd4
Denny Serejo <denny_serejo@hotmail.com>
2019-08-20 15:36:09 -0300

    v2

--- src/app/module-viewer/riverside-step-template/templates/brainstorm/brainstorm.interface.ts
+++ src/app/module-viewer/riverside-step-template/templates/brainstorm/brainstorm.interface.ts
@@ -2,7 +2,7 @@ import { TemplateContentDataBase } from '../template.interface';
 
 export interface BrainstormTemplateData extends TemplateContentDataBase {
   template_params_json: {
-    content: string;
+    description: string;
     title: string;
   };
 }
