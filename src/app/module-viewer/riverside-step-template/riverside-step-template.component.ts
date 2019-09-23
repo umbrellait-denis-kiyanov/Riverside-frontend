68681d664d188173ad65c7948d1b96c3147ed63c
Rinalds Uzkalns <rinalds@integry.net>
2019-09-23 17:59:58 +0300

    Step timer in progress

--- src/app/module-viewer/riverside-step-template/riverside-step-template.component.ts
+++ src/app/module-viewer/riverside-step-template/riverside-step-template.component.ts
@@ -28,7 +28,6 @@ export class RiversideStepTemplateComponent implements OnInit {
     const componentRef = viewContainerRef.createComponent(componentFactory);
     this.templateInstance = (componentRef.instance as TemplateComponent);
     this.templateInstance.data = this.data;
-    console.log(this.data.data.template_params_json);
   }
 
   prepareData() {
--- src/app/module-viewer/riverside-step-template/template-heading/template-heading.component.ts
+++ src/app/module-viewer/riverside-step-template/template-heading/template-heading.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TemplateContentData } from '../templates/template-data.class';
+import { interval, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'template-heading',
@@ -10,13 +12,42 @@ export class TemplateHeadingComponent implements OnInit {
 
   @Input() content: TemplateContentData;
 
+  time = 0;
+  accumulatedTime = 0;
+  timeStart: number;
+  timerInterval: any;
+
+  isTimerOn = false;
+
+  timeInterval$: Observable<void>;
+
+  sub: Subscription;
+
   contentData: any;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(JSON.parse(JSON.stringify(this.content)));
     this.contentData = this.content.data.template_params_json;
+
+    this.timeInterval$ = interval(500).pipe(
+      map(() => {
+        this.time = this.accumulatedTime + Math.floor((Date.now() - this.timeStart) / 1000);
+      })
+    );
+
+    this.toggleTimer();
   }
 
+  toggleTimer() {
+    this.isTimerOn = !this.isTimerOn;
+
+    if (!this.isTimerOn) {
+      this.accumulatedTime = this.time;
+      this.sub.unsubscribe();
+    } else {
+      this.timeStart = Date.now();
+      this.sub = this.timeInterval$.subscribe();
+    }
+  }
 }
