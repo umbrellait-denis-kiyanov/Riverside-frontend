0a37d9510309cde263c090baef9d4102cf1ec443
Rinalds Uzkalns <rinalds@integry.net>
2019-10-14 12:12:05 +0300

    Append sequential numbers to assessment session dates, in case there is more than one completed session in a particular month.

--- src/app/module-viewer/assessment-chart/assessment-chart.component.ts
+++ src/app/module-viewer/assessment-chart/assessment-chart.component.ts
@@ -58,6 +58,8 @@ export class AssessmentChartComponent implements OnInit {
       this.colorScheme.domain = this.colorScheme.domain.concat(this.colors);
     }
 
+    this.colorScheme.domain = this.colorScheme.domain.slice(0, this.series.length);
+
     this.xAxisTickFormatting = (idx: number) => this.labels[idx] || '';
   }
 
