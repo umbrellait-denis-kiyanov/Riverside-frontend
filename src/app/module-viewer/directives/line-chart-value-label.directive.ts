22193c9a9764952a2ccb6b6ecb20b401cc8c5564
Rinalds Uzkalns <livecart@gmail.com>
2019-11-21 02:33:24 +0200

    In assessment overview chart disconnect the last (average) datapoint from the rest of the chart and restyle it as a circle containing the assessment score.

--- src/app/module-viewer/directives/line-chart-value-label.directive.ts
+++ src/app/module-viewer/directives/line-chart-value-label.directive.ts
@@ -1,47 +1,72 @@
-import { Directive, ElementRef, Input, HostListener } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Directive({
   selector: '[appLineChartValueLabels]'
 })
-export class LineChartValueLabelDirective {
+export class LineChartValueLabelDirective implements OnDestroy {
 
   @Input() results: any;
 
+  private changeSize = new BehaviorSubject(null);
+
+  private windowSizeWatch: Subscription;
+
   constructor(private el: ElementRef) {
+    this.el.nativeElement.removeAttribute('displayed');
     const waitForData = setInterval(_ => {
-      if (this.el.nativeElement.querySelector('g.line-series path')) {
+      if (this.el.nativeElement.querySelectorAll('g.line-series path').length === this.results.length) {
         clearInterval(waitForData);
 
-        setTimeout(_ => this.updateLabels(), 200);
+        setTimeout(() => this.updateLabels(), 200);
       }
     });
+
+    this.windowSizeWatch = this.changeSize.pipe(debounceTime(100))
+      .subscribe((value) => {
+        if (!value) {
+          return;
+        }
+        this.el.nativeElement.removeAttribute('displayed');
+        setTimeout(() => this.updateLabels(), 200);
+      });
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    setTimeout(_ => this.updateLabels(), 500);
+    this.changeSize.next(event);
+  }
+
+  ngOnDestroy() {
+    this.windowSizeWatch.unsubscribe();
   }
 
   private updateLabels() {
     const map = [];
-    this.el.nativeElement.querySelectorAll('.datapoint-value').forEach(el => el.remove());
-    const series = this.el.nativeElement.querySelectorAll('g.line-series path');
+    const chartEl = this.el.nativeElement;
+    chartEl.querySelectorAll('.datapoint-value').forEach(el => el.remove());
+    const series = chartEl.querySelectorAll('g.line-series path');
     series.forEach((el, idx) => {
       if (!this.results[idx].name) {
         return;
       }
+
       const points = el.getAttribute('d').substr(1).split('L');
+      const color = el.getAttribute('stroke');
       points.forEach((point, pIdx) => {
         const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
         const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
         g.appendChild(text);
-        g.setAttribute('transform', 'translate(' + point + ')');
+        g.setAttribute('transform', 'translate(' + point.slice(0, -1) + ')');
         g.setAttribute('class', 'datapoint-value');
+
+        const isLast = pIdx === points.length - 1;
 
         // hide nearby points to avoid cluttering the chart with overlapping numbers
         const [x, y] = point.split(',').map(Number);
-        if (map.find(p => Math.abs(x - p[0]) < 30 && Math.abs(y - p[1]) < 30)) {
+        if (!isLast && map.find(p => Math.abs(x - p[0]) < 30 && Math.abs(y - p[1]) < 30)) {
           g.setAttribute('style', 'display: none');
         }
 
@@ -49,15 +74,29 @@ export class LineChartValueLabelDirective {
         text.innerHTML = (dp.formattedValue || dp.value).toString();
         text.setAttribute('stroke-width', '1');
         text.setAttribute('text-anchor', 'start');
-        text.setAttribute('x', '5');
+        text.setAttribute('x', '-10');
         text.setAttribute('y', '5');
-        text.setAttribute('style', 'font-size: 12px; fill: #999;');
+        text.setAttribute('style', 'font-size: 12px; fill: ' + (isLast ? '#fff' : '#999') + ';');
 
         el.parentNode.appendChild(g);
 
+        if (isLast) {
+          const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+          circle.setAttributeNS(null, 'cx', '0');
+          circle.setAttributeNS(null, 'cy', '0');
+          circle.setAttributeNS(null, 'r', '20');
+          circle.setAttributeNS(null, 'style', 'fill: ' + color + ';');
+          g.insertBefore(circle, text);
+        }
+
         map.push([x, y]);
       });
+
+      const avgPoint = points.pop();
+      el.setAttribute('d', 'M' + points.join('L'));
     });
+
+    setTimeout(_ => chartEl.setAttribute('displayed', 'true'), 100);
   }
 
 }
