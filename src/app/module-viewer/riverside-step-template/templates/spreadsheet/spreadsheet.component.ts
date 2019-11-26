2c76f82c1a1975b80eb9683a7d69343213abcd39
Rinalds Uzkalns <rinalds@integry.net>
2019-11-26 18:29:34 +0200

    Spreadsheet in progress (testing PhpSpreadsheet for formula precalculation)

--- src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.component.ts
+++ src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.component.ts
@@ -2,7 +2,7 @@ import { Component, forwardRef } from '@angular/core';
 import { TemplateComponent } from '../template-base.cass';
 import { SpreadsheetTemplateData } from './spreadsheet.interface';
 import * as Handsontable from 'handsontable';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CsvResource } from 'src/app/common/interfaces/module.interface';
 import { tap } from 'rxjs/operators';
 
@@ -16,23 +16,44 @@ export class SpreadsheetComponent extends TemplateComponent {
 
   contentData: SpreadsheetTemplateData['template_params_json'];
 
-  csv$: Observable<CsvResource>;
+  csvSub: Subscription;
 
   csv: CsvResource;
 
-  types
+  types: string[][];
 
   init() {
-    console.log(this.data.data.template_params_json.apiResource);
-    this.csv$ = this.moduleService.getCsv(0, this.data.data.template_params_json.apiResource)
-                  .pipe(tap(data => {
-                    this.csv = data;
-                    this.types = data.data.reduce((rows, row) => {
+    this.csvSub = this.moduleService.getCsv(0, this.data.data.template_params_json.apiResource)
+                  .pipe(
+                    tap(data => {
+                      this.types = data.data.map((row, rowIndex) => row.map((cell, cellIndex) => {
+                        if (null === cell) {
+                          return null;
+                        }
 
-                      return rows;
-                    }, {});
-                    console.log(data);
-                  }));
+                        let cellType = null;
+
+                        if (cell.match(/^[0-9]+\%$/)) {
+                          cell = parseInt(cell, 10).toString();
+                          cellType = 'percent';
+                        } else
+                        if (cell.match(/^\$[,0-9]+$/)) {
+                          cell = parseInt(cell.split(/[^0-9]/).join(''), 10).toString();
+                          cellType = 'currency';
+                        }
+
+                        data.data[rowIndex][cellIndex] = cell;
+
+                        return cellType;
+                      }));
+
+                      this.csv = data;
+
+                      console.log(this.types);
+
+                    }
+                  ))
+                  .subscribe();
   }
 
   getDescription() {
@@ -44,7 +65,7 @@ export class SpreadsheetComponent extends TemplateComponent {
   }
 
   formatCell(row, column, data, smth, snth2) {
-    console.log(row, column, data, smth, snth2);
+    // console.log(row, column, data, smth, snth2);
 
     return {};
   }
