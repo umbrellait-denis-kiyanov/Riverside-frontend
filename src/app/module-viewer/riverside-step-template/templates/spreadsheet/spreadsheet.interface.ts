01e5be99c6ad93f8fd3caa8a5ec70ecebf4ab7c3
Rinalds Uzkalns <rinalds@integry.net>
2019-11-28 18:02:46 +0200

    Revenue Model content

--- src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.interface.ts
+++ src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.interface.ts
@@ -3,8 +3,8 @@ import { TemplateContentDataBase } from '../template.interface';
 export interface SpreadsheetTemplateData extends TemplateContentDataBase {
   template_params_json: {
     description: string;
-    title: string;
     apiResource: string;
     visibleRows: string;
+    title: string;
   };
 }
