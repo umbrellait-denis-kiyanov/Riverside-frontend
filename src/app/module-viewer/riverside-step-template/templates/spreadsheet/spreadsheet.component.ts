a3def3ed6f5af3d053984b2b2121b08e96fe8bb6
Rinalds Uzkalns <rinalds@integry.net>
2019-11-28 16:38:08 +0200

    Lock read-only cells

--- src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.component.ts
+++ src/app/module-viewer/riverside-step-template/templates/spreadsheet/spreadsheet.component.ts
@@ -22,53 +22,71 @@ export class SpreadsheetComponent extends TemplateComponent {
 
   types: string[][];
 
+  cellSettings: { editable: boolean; className: string; }[][];
+
   hiddenRows = {
     rows: [],
   };
 
   init() {
     const contentData = this.data.data.template_params_json;
-    this.sheetSub = this.moduleService.getSpreadsheet(0, contentData.apiResource)
-                  .pipe(
-                    tap(data => {
-                      this.types = data.data.map((row, rowIndex) => row.map((cell, cellIndex) => {
-                        if (null === cell) {
-                          return null;
-                        }
 
-                        let cellType = null;
+    this.sheetSub =
+      this.moduleService.getSpreadsheet(0, contentData.apiResource)
+        .pipe(
+          tap(data => {
+            this.cellSettings = data.data.map((row, rowIndex) => row.map((cell, cellIndex) => ({editable: false, className: ''})));
 
-                        if (cell.match(/^[0-9]+\%$/)) {
-                          cell = parseInt(cell, 10).toString();
-                          cellType = 'percent';
-                        } else
-                        if (cell.match(/^\$[,0-9]+$/)) {
-                          cell = parseInt(cell.split(/[^0-9]/).join(''), 10).toString();
-                          cellType = 'currency';
-                        }
+            this.types = data.data.map((row, rowIndex) => row.map((cell, cellIndex) => {
+              if (null === cell) {
+                return null;
+              }
 
-                        data.data[rowIndex][cellIndex] = cell;
+              let cellType = null;
 
-                        return cellType;
-                      }));
+              if (cell.match(/^[0-9]+\%$/)) {
+                cell = parseInt(cell, 10).toString();
+                cellType = 'percent';
+              } else
+              if (cell.match(/^\$[,0-9]+$/)) {
+                cell = parseInt(cell.split(/[^0-9]/).join(''), 10).toString();
+                cellType = 'currency';
+              }
 
-                      this.sheet = data;
+              data.data[rowIndex][cellIndex] = cell;
 
-                      if (contentData.visibleRows) {
-                        this.hiddenRows.rows = Array.from(Array(data.data.length).keys());
+              return cellType;
+            }));
 
-                        this.textContent(contentData.visibleRows).split(',').map(intv => {
-                          const highLow = intv.split('-');
-                          for (let idx = Number(highLow[0]) - 1; idx <= Number(highLow[highLow.length - 1]) - 1; idx++) {
-                            this.hiddenRows.rows.splice(this.hiddenRows.rows.indexOf(idx), 1);
-                          }
-                        });
-                      } else {
-                        this.hiddenRows.rows = [];
-                      }
-                    }
-                  ))
-                  .subscribe();
+            this.cellSettings = data.meta.editable.reduce((editable, range) => {
+              const row = parseInt(range, 10);
+              const cellRange = range.split(/[0-9]/).join('').split('-');
+
+              for (let idx = cellRange[0].charCodeAt(0) - 65; idx <= cellRange[cellRange.length - 1].charCodeAt(0) - 65; idx++) {
+                editable[row - 1][idx].editable = true;
+              }
+
+              return editable;
+            }, this.cellSettings);
+
+            this.sheet = data;
+
+            if (contentData.visibleRows) {
+              this.hiddenRows.rows = Array.from(Array(data.data.length).keys());
+
+              this.textContent(contentData.visibleRows).split(',').map(intv => {
+                const highLow = intv.split('-');
+                for (let idx = Number(highLow[0]) - 1; idx <= Number(highLow[highLow.length - 1]) - 1; idx++) {
+                  this.hiddenRows.rows.splice(this.hiddenRows.rows.indexOf(idx), 1);
+                }
+              });
+            } else {
+              this.hiddenRows.rows = [];
+            }
+          }
+        ))
+        .subscribe();
+
   }
 
   getDescription() {
@@ -79,9 +97,11 @@ export class SpreadsheetComponent extends TemplateComponent {
     return 'Spreadsheet';
   }
 
-  formatCell(row, column, data, smth, snth2) {
-    // console.log(row, column, data, smth, snth2);
+  formatCell(row, column, prop) {
+    const cell = {} as any;
 
-    return {};
+    cell.readOnly = !this.cellSettings[row][column].editable;
+
+    return cell;
   }
 }
