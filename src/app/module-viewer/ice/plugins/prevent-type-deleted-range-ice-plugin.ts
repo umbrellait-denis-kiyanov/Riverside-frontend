6dd1e72db8d4a742f5899ba0006c9dc9e6ce457e
Rinalds Uzkalns <rinalds@integry.net>
2020-01-16 14:19:19 +0200

    Improved PreventTypeDeletedRangePlugin to allow entering new text right before or after a deleted block. In case the user attempts to type inside the deleted block, cursor is automatically moved to the end of the block, the same applies for pasting.

--- src/app/module-viewer/ice/plugins/prevent-type-deleted-range-ice-plugin.ts
+++ src/app/module-viewer/ice/plugins/prevent-type-deleted-range-ice-plugin.ts
@@ -1,18 +1,12 @@
 import IceInputPlugin from './ice-input-plugin';
 
-// disallow typing in the middle of a range of deleted text
-// @todo - automatically start a new range after the deleted text instead
+// disallow typing or pasting in the middle of a range of deleted text
+// automatically start a new range after the deleted text instead
 export default class PreventTypeDeletedRangePlugin extends IceInputPlugin {
     keyDown(e: KeyboardEvent) {
-        // home/end, arrow keys
-        if (e.which >= 35 && e.which <= 40) {
-            return true;
-        }
-
-        if (!this.isCurrentRangeEditable())
-        {
-            this.stopEvent(e);
-            return false;
+        //    backspace         delete              home/end, arrow keys
+        if (e.which !== 8 && e.which !== 46 && !(e.which >= 35 && e.which <= 40) && !this.isCurrentRangeEditable()) {
+            this.createNewRange();
         }
 
         return true;
@@ -22,9 +16,23 @@ export default class PreventTypeDeletedRangePlugin extends IceInputPlugin {
         element.addEventListener('paste', e => {
             if (!this.isCurrentRangeEditable())
             {
-                this.stopEvent(e);
+                this.createNewRange();
             }
         });
+    }
+
+    private createNewRange() {
+        const range = this.ice.getCurrentRange();
+
+        const delElement = range.nativeRange.commonAncestorContainer.parentElement;
+        const newRange = this.ice.selection.createRange();
+        const placeholder = document.createElement('span');
+
+        // insert placeholder before or after deleted text depending on cursor position
+        delElement.parentNode.insertBefore(placeholder, 0 === range.endOffset ? delElement : delElement.nextSibling);
+
+        newRange.setStart(placeholder, 0);
+        this.ice.selection.addRange(newRange);
     }
 
     private isCurrentRangeEditable() {
