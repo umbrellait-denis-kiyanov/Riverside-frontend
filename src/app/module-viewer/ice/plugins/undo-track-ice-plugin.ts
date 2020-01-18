bcfbfbd67d6435b89fc7c5b7d9d6330db57912a0
Rinalds Uzkalns <livecart@gmail.com>
2020-01-18 03:54:50 +0200

    Finished UndoTrackPlugin - implements undo/redo functionality. Todo - Mac shortcuts should be added.

--- src/app/module-viewer/ice/plugins/undo-track-ice-plugin.ts
+++ src/app/module-viewer/ice/plugins/undo-track-ice-plugin.ts
@@ -1,51 +1,60 @@
 import IceInputPlugin from './ice-input-plugin';
 import { BehaviorSubject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, skip } from 'rxjs/operators';
+import { debounceTime, filter, skip, takeWhile, finalize } from 'rxjs/operators';
+
+type Editor = HTMLElement & {
+                change$: BehaviorSubject<string>;
+                stack: string[];
+                stackIndex: number;
+              }
 
 // implement Undo/Redo functionality
 export default class UndoTrackPlugin extends IceInputPlugin {
 
-    protected addEventListeners(editor) {
+    protected addEventListeners(editor: Editor) {
 
-        editor.change$ = new BehaviorSubject<string>('');
+        editor.change$ = new BehaviorSubject('');
+
+        editor.stack = [];
+
+        const observer = new MutationObserver(_ => editor.change$.next(editor.innerHTML));
+
+        observer.observe(editor, {
+            attributes: true,
+            subtree: true,
+            childList: true,
+            characterData: true
+          });
 
         editor.change$.pipe(
             skip(1),
-            debounceTime(1000)
+            debounceTime(500),
+            filter(html => html !== null && editor.stack[editor.stackIndex] !== html),
+            takeWhile(_ => !!editor.parentElement),
+            finalize(() => observer.disconnect())
         ).subscribe(html => {
-            editor.stack = editor.stack.slice(0, editor.stackIndex + 1);
-
-            if (editor.stack[editor.stackIndex] !== html) {
-                editor.stack.push(html);
-                editor.stackIndex = editor.stack.length;
-                console.log('Adding to stack', editor.stackIndex);
-            }
+            editor.stack = editor.stack.slice(0, editor.stackIndex + 2);
+            editor.stack.push(html);
+            editor.stackIndex = editor.stack.length - 1;
         });
     }
 
     keyDown(e: KeyboardEvent) {
-        const editor = e.target as any;
+        if (e.ctrlKey && (e.key === 'z' || e.key === 'y')) {
+            const editor = e.target as Editor;
 
-        if (!editor.stackIndex) {
-            editor.stack = [editor.innerHTML];
-        }
+            if (e.key === 'z') {
+                editor.stack[editor.stackIndex] = editor.innerHTML;
+                editor.stackIndex = Math.max(0, editor.stackIndex - 1);
+            }
 
-        if (e.ctrlKey && e.key === 'z') {
-            editor.stackIndex = Math.max(0, editor.stackIndex - 1);
+            if (e.key === 'y') {
+                editor.stackIndex = Math.min(editor.stack.length - 1, editor.stackIndex + 1);
+            }
+
             editor.innerHTML = editor.stack[editor.stackIndex];
-            console.log('Restoring from stack', editor.stackIndex);
-            return true;
+            editor.change$.next(null);
         }
-
-        if (e.ctrlKey && e.key === 'y') {
-            editor.stackIndex = Math.min(editor.stack.length - 1, editor.stackIndex + 1);
-            editor.innerHTML = editor.stack[editor.stackIndex];
-            return true;
-        }
-
-        editor.change$.next(editor.innerHTML);
-
-        // console.log(editor.stack);
 
         return true;
     }
