3b208e490044eb246d4d8209789f3f7e0b219ed2
alexeyovsyanikov <alexey.ovsyanikov@umbrellait.com>
2020-02-11 21:11:23 +0300

    File uploader development

--- src/app/module-viewer/riverside-step-template/templates/file-uploader/file-uploader.component.ts
+++ src/app/module-viewer/riverside-step-template/templates/file-uploader/file-uploader.component.ts
@@ -1,7 +1,8 @@
-import {Component, ElementRef, forwardRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, forwardRef, HostListener, Inject, OnInit, ViewChild} from '@angular/core';
 import {TemplateComponent} from '../template-base.class';
 import {FileUploaderTemplateData, TemplateParams} from '.';
 import { faFileExcel , faFileImage } from '@fortawesome/free-solid-svg-icons';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-file-uploader',
@@ -12,14 +13,18 @@ import { faFileExcel , faFileImage } from '@fortawesome/free-solid-svg-icons';
 export class FileUploaderComponent extends TemplateComponent {
 
   @ViewChild('customFile') fileInput: ElementRef;
+  toastr: ToastrService;
 
   contentData: FileUploaderTemplateData['template_params_json'];
   params = TemplateParams;
 
-  availableFileTypes = [
+  availableImagesTypes = [
     'image/jpeg',
-    'image/png',
-    'text/csv'
+    'image/png'
+  ];
+
+  availableSpreadsheetTypes = [
+      'text/csv'
   ];
 
   isFileChosen = false;
@@ -29,6 +34,11 @@ export class FileUploaderComponent extends TemplateComponent {
   faFileExcelIcon = faFileExcel;
   faFileImageIcon = faFileImage;
   imgURL: any;
+  fileObjectToUpload: {
+    filename: string,
+    filetype: string,
+    value: any
+  };
 
   getDescription(): string {
     return 'File uploader';
@@ -43,9 +53,8 @@ export class FileUploaderComponent extends TemplateComponent {
   }
 
   protected init() {
-
     this.contentData = this.data.data.template_params_json as FileUploaderTemplateData['template_params_json'];
-
+    this.toastr = this.injectorObj.get(ToastrService);
   }
 
   onFileSectionClick() {
@@ -58,45 +67,70 @@ export class FileUploaderComponent extends TemplateComponent {
 
     const { dataTransfer } = $event;
 
-    if (dataTransfer.files.length === 1 && this.availableFileTypes.indexOf(dataTransfer.files[0].type) !== -1) {
+    if (dataTransfer.files.length === 1 && this.isFileExtensionValid(dataTransfer.files[0])) {
       this.fileInput.nativeElement.files = dataTransfer.files;
       this.isFileChosen = true;
-      this.fileName = dataTransfer.files[0].name;
-      this.fileSize = dataTransfer.files[0].size / 1024;
-      if ( this.fileSize >= 1024 ) {
-        this.sizeSuffix = 'MB';
-        this.fileSize /= 1024;
-      } else {
-        this.sizeSuffix = 'KB';
-      }
-      console.log(this.fileInput.nativeElement.files);
+      this.formatFileInfo(this.fileInput.nativeElement.files[0]);
+      this.prepareFileToUpload(this.fileInput.nativeElement.files[0]);
     } else {
-      alert('Only one file with type: ' + this.availableFileTypes);
+      this.toastr.error('File format is incorrect');
     }
   }
 
   removeFileToUpload($event: MouseEvent) {
     $event.stopPropagation();
     this.isFileChosen = false;
-    // this.fileInput.nativeElement.files = new FileList();
   }
 
   onFileChange() {
-    const reader = new FileReader();
-    if ( this.fileInput.nativeElement.files && this.fileInput.nativeElement.files.length === 1) {
-      const file = this.fileInput.nativeElement.files[0];
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        this.imgURL = reader.result;
-        this.isFileChosen = true;
-        if (typeof reader.result === 'string') {
-          console.log({
-            filename: file.name,
-            filetype: file.type,
-            value: reader.result.split(',')[1]
-          });
-        }
-      };
+
+    const files = this.fileInput.nativeElement.files;
+
+    if ( files && files.length === 1 && this.isFileExtensionValid(files[0])) {
+      this.formatFileInfo(files[0]);
+      this.prepareFileToUpload(files[0]);
+    } else {
+      this.toastr.error('File format is incorrect');
     }
   }
+
+  formatFileInfo(file: File) {
+    this.fileName = file.name;
+    this.fileSize = file.size / 1024;
+    if ( this.fileSize >= 1024 ) {
+      this.sizeSuffix = 'MB';
+      this.fileSize /= 1024;
+    } else {
+      this.sizeSuffix = 'KB';
+    }
+  }
+
+  isFileExtensionValid(file: File): boolean {
+    if ( this.contentData.step_select === 'Image' ) {
+      return this.availableImagesTypes.indexOf(file.type) !== -1;
+    }
+    return this.availableSpreadsheetTypes.indexOf(file.type) !== -1;
+  }
+
+  prepareFileToUpload(file: File) {
+
+    const reader = new FileReader();
+    this.isFileChosen = true;
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if ( this.contentData.step_select === 'Image' ) {
+        this.imgURL = reader.result;
+      } else {
+        this.imgURL = null;
+      }
+      if (typeof reader.result === 'string') {
+        this.fileObjectToUpload = {
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        };
+      }
+    };
+  }
+
 }
