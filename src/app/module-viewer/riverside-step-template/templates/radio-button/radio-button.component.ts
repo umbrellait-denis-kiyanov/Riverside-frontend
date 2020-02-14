105b37b883823f5bde32f1a831ac10ecbc0fa951
alexeyovsyanikov <alexey.ovsyanikov@umbrellait.com>
2020-02-14 13:19:04 +0300

    before checkout to calendar branch

--- src/app/module-viewer/riverside-step-template/templates/radio-button/radio-button.component.ts
+++ src/app/module-viewer/riverside-step-template/templates/radio-button/radio-button.component.ts
@@ -2,6 +2,7 @@ import {Component, forwardRef, OnInit} from '@angular/core';
 import {TemplateComponent} from '../template-base.class';
 import {TemplateParams , RadiobuttonTemplateData} from '.';
 import {Item} from './Item';
+import {MatRadioChange} from '@angular/material';
 
 @Component({
   selector: 'app-radio-button',
@@ -12,6 +13,10 @@ import {Item} from './Item';
 export class RadioButtonComponent extends TemplateComponent {
 
   items: Item[];
+
+  inputIds: {
+    radioButtons: string[]
+  };
 
   params = TemplateParams;
 
@@ -30,8 +35,23 @@ export class RadioButtonComponent extends TemplateComponent {
     return true;
   }
 
+  validate(): boolean {
+    return this.userChoice && this.contentData.require_selection;
+  }
+
   protected init() {
     this.contentData = this.data.data.template_params_json as RadiobuttonTemplateData['template_params_json'];
     this.items = this.contentData.options;
+    console.log(this.getInput('userChoiceRadio'));
+    // this.getInput()
+    // this.inputIds = {
+    //   radioButtons: this.items.map(item => persona.split('_').join('_behavior_') + suffix)
+    // };
+    console.log(this.inputs);
+  }
+
+  onRadioChange($event: MatRadioChange) {
+    console.log($event);
+    console.log(this.getInput('userChoiceRadio'));
   }
 }
