213e109852797d7c0c057ce2ba1a0a8994235e4c
Anton Lavrik <anton.lavrik@deliveryweb.ru>
2020-02-14 18:32:02 +0300

    #38 future: add options for template

--- src/app/module-viewer/riverside-step-template/templates/content-map/content-map.component.ts
+++ src/app/module-viewer/riverside-step-template/templates/content-map/content-map.component.ts
@@ -2,6 +2,7 @@ import { Component, forwardRef } from '@angular/core';
 import { TemplateComponent } from '../template-base.class';
 import { ContentMapTemplateData } from '.';
 import txt from '!!raw-loader!./index.ts';
+import { BrainstormTemplateData } from "../brainstorm";
 
 declare interface ContentMapInputData {
   hasData: boolean;
@@ -15,7 +16,7 @@ declare interface ContentMapInputData {
 })
 export class ContentMapComponent extends TemplateComponent {
   params = txt;
-
+  inputState: number;
   contentData: ContentMapTemplateData['template_params_json'];
 
   getDescription() {
@@ -28,6 +29,14 @@ export class ContentMapComponent extends TemplateComponent {
 
   get input() {
     return this.getInput('content_map');
+  }
+
+  protected init() {
+    // @ts-ignore - template_params_json.inputs property causes error with TypeScript 3.1
+    this.contentData = this.data.data.template_params_json as ContentMapTemplateData['template_params_json'];
+
+    this.inputState = Number(this.contentData.typeOfMap_select.substr(0, 1));
+    this.buyerPersonasList$ = this.buyerPersonasService.getBuyerPersonas();
   }
 
   set contentMapInputData(contentMapInputData: ContentMapInputData) {
--- src/app/module-viewer/riverside-step-template/templates/content-map/index.ts
+++ src/app/module-viewer/riverside-step-template/templates/content-map/index.ts
@@ -3,7 +3,7 @@ import { TemplateContentDataBase } from '../template.interface';
 export interface ContentMapTemplateData extends TemplateContentDataBase {
   template_params_json: {
     description: string;
-    typeOfMap_select: 'simple_map' | 'buyer_personas';
+    typeOfMap_select: '1_simple_map' | '2_buyer_personas';
     title: string;
     inputs: string;
   };
